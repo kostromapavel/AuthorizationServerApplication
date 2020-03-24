5bd182a3b4d6f91af90d88098e646cf8f2c83437
kostromapavel@gmail.com <Almoda93>
2020-03-24 14:57:22 +0300

    Revert "secure change"
    This reverts commit 34ef0423

--- src/main/java/com/okta/spring/AuthorizationServerApplication/AuthServerConfig.java
+++ src/main/java/com/okta/spring/AuthorizationServerApplication/AuthServerConfig.java
@@ -1,20 +1,14 @@
 package com.okta.spring.AuthorizationServerApplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -27,25 +21,11 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Value("${user.oauth.redirectUris}")
     private String RedirectURLs;
 
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    @Qualifier("authenticationManagerBean")
-    private AuthenticationManager authenticationManager;
-
-    public AuthServerConfig() {
+    public AuthServerConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
-
-    @Override
-    public void configure(
-            AuthorizationServerEndpointsConfigurer endpoints)
-            throws Exception {
-
-        endpoints
-                .authenticationManager(authenticationManager);
-    }
-
 
     @Override
     public void configure(
@@ -58,20 +38,10 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient(ClientID)
-                .secret(passwordEncoder.encode(ClientSecret))
+                .secret(ClientSecret)
                 .authorizedGrantTypes("authorization_code")
                 .scopes("openid")
                 .autoApprove(true)
                 .redirectUris(RedirectURLs);
-/*        clients
-                .inMemory()
-                .withClient(ClientID).secret(passwordEncoder.encode("123456"))
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-                .authorities("READ_ONLY_CLIENT")
-                .scopes("openid")
-                .resourceIds("oauth2-resource")
-                .redirectUris(RedirectURLs)
-                .accessTokenValiditySeconds(120)
-                .refreshTokenValiditySeconds(240000);*/
     }
 }
